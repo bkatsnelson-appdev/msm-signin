d25488e3e35533db7bf5d51c5195df260f38ef50
beatricek222 <beatricek222@gmail.com>
2022-11-18 21:53:34 +0000

    Tailoring bookmarks page

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -17,6 +17,7 @@ class Movie < ApplicationRecord
   has_many :characters
 
   has_many :cast, :through => :characters, :source => :actor
-
-  validates(:title, {:presence => true})
+  has_many :bookmarks
+  has_many :bookmarkers, :through => :bookmarks, :source => users
+  validates(:title, { :presence => true })
 end
